331dec848cd15a42139615d96d4cf51592a3384b
edgy edgeworth <suan5553@gmail.com>
2025-08-06 13:13:01 +0000

    added kernel 6.16 variant

--- flake.nix
+++ flake.nix
@@ -28,7 +28,15 @@ extra-substitors = [
     linux = linux_6_15;
     stdenv = clangStdenv;
     };
-
+    linux_6_16-tkt  =   callPackage  ./tkt.nix {
+    linux = linux_6_16;
+    };
+     linux_6_16-tkt-clang  =   callPackage  ./tkt.nix {
+    linux = linux_6_16;
+    stdenv = clangStdenv;
+    };
+    linuxPackages_6_16-tkt =  linuxPackagesFor self.packages.x86_64-linux.linux_6_16-tkt;
+    linuxPackages_6_16-tkt_clang =  linuxPackagesFor self.packages.x86_64-linux.linux_6_16-tkt-clang;
     linuxPackages_6_15-tkt =  linuxPackagesFor self.packages.x86_64-linux.linux_6_15-tkt;
     linuxPackages_6_15-tkt_clang =  linuxPackagesFor self.packages.x86_64-linux.linux_6_15-tkt-clang;
 
