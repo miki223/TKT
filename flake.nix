a610fb73c268b1fb555f6614789afdf58c747449
edgy edgeworth <suan5553@gmail.com>
2025-08-06 12:34:45 +0000

    changed api of tkt.nix which required the flake to change slightly

--- flake.nix
+++ flake.nix
@@ -2,27 +2,22 @@
   description = "A very basic flake";
 
   inputs = {
+    nix-filter .url = "github:numtide/nix-filter";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    tktRepo = {
-    url = "path:./.";
-   flake = false;
-   };
   };
 
 
-  outputs = { self, nixpkgs,tktRepo,... }: {
+  outputs = { self, nixpkgs,... }: {
 
 
     packages.x86_64-linux   =  with  nixpkgs.legacyPackages.x86_64-linux;  {
     linux_6_15-tkt  =   callPackage  ./tkt.nix {
     linux = linux_6_15;
-    inherit tktRepo;
     };
      linux_6_15-tkt-clang  =   callPackage  ./tkt.nix {
     linux = linux_6_15;
     stdenv = clangStdenv;
-    inherit tktRepo;
     };
 
     linuxPackages_6_15-tkt =  linuxPackagesFor self.packages.x86_64-linux.linux_6_15-tkt;
--- tkt.nix
+++ tkt.nix
@@ -1,61 +1,61 @@
 
-{ lib, fetchFromGitHub, linuxManualConfig, linux,stdenv , features ? {},kernelPatches ? [],randstructSeed ? "",  tktRepo ? (fetchFromGitHub {
-   owner  = "ETJAKEOC";
-   repo = "TKT";
-    rev =  "1ac91e8";
-   sha256 = "X8s++DlAiVCqyBREycedxJQs6TQawoKSCni5Ez4QSVc=";}
-   )}:
+{ lib, fetchFromGitHub, linuxManualConfig, linux,stdenv , features ? {},kernelPatches ? [],randstructSeed ? ""}:
+
 
 
 let
 
   kversionNoPatch = lib.versions.majorMinor linux.version;
+  ksources  = with  lib.fileset; toSource {
+  root = ./.;
+  fileset =( union ./kconfigs/${kversionNoPatch}  ./kpatches/${kversionNoPatch});
+  };
   compiler = {
   name = if stdenv.cc.isGNU then "GCC" else  if stdenv.cc.isClang then "LLVM" else "unknown";
   version = stdenv.cc.version;
   };
-in
+  in
 
 
 linuxManualConfig  {
   version = "${linux.version}-tkt";
   allowImportFromDerivation = true;
-  configfile = "${tktRepo}/kconfigs/${kversionNoPatch}/config.x86_64";
+  configfile = "${ksources}/kconfigs/${kversionNoPatch}/config.x86_64";
   inherit (linux) src;
  inherit  stdenv;
  extraMakeFlags = ["LOCALVERSION=-tkt-${compiler.name}_${compiler.version}"];
  kernelPatches = [
         {
         name = "add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER";
-        patch =   "${tktRepo}/kpatches/${kversionNoPatch}/0001-add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER-by.patch";
+        patch =   "${ksources}/kpatches/${kversionNoPatch}/0001-add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER-by.patch";
         }
         {
         name = "bore";
-        patch  =  "${tktRepo}/kpatches/${kversionNoPatch}/0001-bore.patch";
+        patch  =  "${ksources}/kpatches/${kversionNoPatch}/0001-bore.patch";
         }
         {
        name = "clear-patches";
-       patch  =  "${tktRepo}/kpatches/${kversionNoPatch}/0002-clear-patches.patch";
+       patch  =  "${ksources}/kpatches/${kversionNoPatch}/0002-clear-patches.patch";
         }
         {
         name = "glitched-base ";
-        patch  =  "${tktRepo}/kpatches/${kversionNoPatch}/0003-glitched-base.patch";
+        patch  =  "${ksources}/kpatches/${kversionNoPatch}/0003-glitched-base.patch";
         }
         {
         name = "glitched-cfs";
-        patch = " ${tktRepo}/kpatches/${kversionNoPatch}//0003-glitched-cfs.patch";
+        patch = " ${ksources}/kpatches/${kversionNoPatch}//0003-glitched-cfs.patch";
         }
         {
         name = "add-acs-overrides_iommu";
-        patch = " ${tktRepo}/kpatches/${kversionNoPatch}/0006-add-acs-overrides_iommu.patch";
+        patch = " ${ksources}/kpatches/${kversionNoPatch}/0006-add-acs-overrides_iommu.patch";
         }
         {
       name = "0006-add-acs-";
-      patch = " ${tktRepo}/kpatches/${kversionNoPatch}//0012-misc-additions.patch";
+      patch = " ${ksources}/kpatches/${kversionNoPatch}//0012-misc-additions.patch";
         }
      {
      name = "OpenRGB";
-     patch = "${tktRepo}/kpatches/${kversionNoPatch}//0014-OpenRGB.patch";
+     patch = "${ksources}/kpatches/${kversionNoPatch}//0014-OpenRGB.patch";
      }
 
   ];
