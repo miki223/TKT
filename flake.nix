e7139e36958d1a3fc39f982752147e287fd565ac
edgy edgeworth <suan5553@gmail.com>
2025-08-07 13:15:53 +0000

    updated flake to use thk-utils

--- flake.nix
+++ flake.nix
@@ -11,37 +11,19 @@ extra-substitors = [
   ];
 };
   inputs = {
-    nix-filter .url = "github:numtide/nix-filter";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
 
-  outputs = { self, nixpkgs,... }: {
-
-
-    packages.x86_64-linux   =  with  nixpkgs.legacyPackages.x86_64-linux;  {
-    linux_6_15-tkt  =   callPackage  ./tkt.nix {
-    linux = linux_6_15;
-    };
-     linux_6_15-tkt-clang  =   callPackage  ./tkt.nix {
-    linux = linux_6_15;
-    stdenv = clangStdenv;
-    };
-    linux_6_16-tkt  =   callPackage  ./tkt.nix {
-    linux = linux_6_16;
-    };
-     linux_6_16-tkt-clang  =   callPackage  ./tkt.nix {
-    linux = linux_6_16;
-    stdenv = clangStdenv;
-    };
-    linuxPackages_6_16-tkt =  linuxPackagesFor self.packages.x86_64-linux.linux_6_16-tkt;
-    linuxPackages_6_16-tkt_clang =  linuxPackagesFor self.packages.x86_64-linux.linux_6_16-tkt-clang;
-    linuxPackages_6_15-tkt =  linuxPackagesFor self.packages.x86_64-linux.linux_6_15-tkt;
-    linuxPackages_6_15-tkt_clang =  linuxPackagesFor self.packages.x86_64-linux.linux_6_15-tkt-clang;
-
-    };
-
+  outputs = { self, nixpkgs,... }:
+  let
+  pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  tktUtils = pkgs.callPackage ./tkt-utils.nix {};
+  inherit  (tktUtils) mkTKTForKernels;
+  in
+  {
+    packages.x86_64-linux   = with pkgs;  mkTKTForKernels [linux_6_15 linux_6_16 linux_6_1];
     } ;
 
 
