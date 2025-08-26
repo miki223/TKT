7d17bc1f5837335180bdb63b258ef7da113b10ea
edgy edgeworth <suan5553@gmail.com>
2025-08-26 09:25:40 +0000

    ran code formatter & fixed trailing spaces

--- tkt-utils.nix
+++ tkt-utils.nix
@@ -1,32 +1,45 @@
-{callPackage,lib,stdenv,clangStdenv,linuxPackagesFor }:let
-mkTKT =  callPackage ./tkt.nix;
-mkTKTForKernels = kernels: lib.mergeAttrsList (map mkTKTForKernel kernels);
-mkTKTForKernel = kernel: let
-  _major_minor_ersion = lib.replaceString "." "_" (lib.versions.majorMinor  kernel.version);
-  _nixosPackageFor =  kernel: linuxPackagesFor kernel;
-  kernels = {
-  "linux_${_major_minor_ersion}-tkt" = mkTKT {
-  linux = kernel;
-  inherit stdenv;
-  };
-  "linux_${_major_minor_ersion}-tkt_clang" = mkTKT {
-  linux = kernel;
-  stdenv =  clangStdenv;
-  };
+{
+  callPackage,
+  lib,
+  stdenv,
+  clangStdenv,
+  linuxPackagesFor,
+}:
+let
+  mkTKT = callPackage ./tkt.nix;
+  mkTKTForKernels = kernels: lib.mergeAttrsList (map mkTKTForKernel kernels);
+  mkTKTForKernel =
+    kernel:
+    let
+      _major_minor_ersion = lib.replaceString "." "_" (lib.versions.majorMinor kernel.version);
+      _nixosPackageFor = kernel: linuxPackagesFor kernel;
+      kernels = {
+        "linux_${_major_minor_ersion}-tkt" = mkTKT {
+          linux = kernel;
+          inherit stdenv;
+        };
+        "linux_${_major_minor_ersion}-tkt_clang" = mkTKT {
+          linux = kernel;
+          stdenv = clangStdenv;
+        };
 
+      };
+    in
+    {
+      "linux_${_major_minor_ersion}-tkt_clang" = kernels."linux_${_major_minor_ersion}-tkt_clang";
+      "linux_${_major_minor_ersion}-tkt" = kernels."linux_${_major_minor_ersion}-tkt";
+      "linuxPackages_${_major_minor_ersion}-tkt" =
+        linuxPackagesFor
+          kernels."linux_${_major_minor_ersion}-tkt";
+      "linuxPackages_${_major_minor_ersion}-tkt_clang" =
+        linuxPackagesFor
+          kernels."linux_${_major_minor_ersion}-tkt_clang";
 
-  };
-  in  {
-    "linux_${_major_minor_ersion}-tkt_clang" = kernels. "linux_${_major_minor_ersion}-tkt_clang" ;
-    "linux_${_major_minor_ersion}-tkt" = kernels."linux_${_major_minor_ersion}-tkt";
-    "linuxPackages_${_major_minor_ersion}-tkt" = linuxPackagesFor kernels."linux_${_major_minor_ersion}-tkt";
-    "linuxPackages_${_major_minor_ersion}-tkt_clang" = linuxPackagesFor kernels."linux_${_major_minor_ersion}-tkt_clang";
+    };
 
-  };
-
-in {
-inherit mkTKT;
-inherit mkTKTForKernel;
-inherit mkTKTForKernels;
+in
+{
+  inherit mkTKT;
+  inherit mkTKTForKernel;
+  inherit mkTKTForKernels;
 }
-
--- tkt.nix
+++ tkt.nix
@@ -1,86 +1,108 @@
-
-{ lib, fetchFromGitHub, linuxManualConfig, linux,stdenv , features ? {},kernelPatches ? [],randstructSeed ? "",withBore ? true,minimal-modules ? true}:
-
-
+{
+  lib,
+  fetchFromGitHub,
+  linuxManualConfig,
+  linux,
+  stdenv,
+  features ? { },
+  kernelPatches ? [ ],
+  randstructSeed ? "",
+  withBore ? true,
+  minimal-modules ? true,
+}:
 
 let
 
   kversionNoPatch = lib.versions.majorMinor linux.version;
-  ksources  = with  lib.fileset; toSource {
-  root = ./.;
-  fileset =( union ./kconfigs/${kversionNoPatch}  ./kpatches/${kversionNoPatch});
-  };
+  ksources =
+    with lib.fileset;
+    toSource {
+      root = ./.;
+      fileset = (union ./kconfigs/${kversionNoPatch} ./kpatches/${kversionNoPatch});
+    };
   compiler = {
-  name = if stdenv.cc.isGNU then "GCC" else  if stdenv.cc.isClang then "LLVM" else "unknown";
-  version = stdenv.cc.version;
+    name =
+      if stdenv.cc.isGNU then
+        "GCC"
+      else if stdenv.cc.isClang then
+        "LLVM"
+      else
+        "unknown";
+    version = stdenv.cc.version;
   };
-  patchdir  = "${ksources}/kpatches/${kversionNoPatch}";
-  scheduler = if withBore then [
-   {
-        name = "bore";
-        patch  =  "${patchdir}/0001-bore.patch";
+  patchdir = "${ksources}/kpatches/${kversionNoPatch}";
+  scheduler =
+    if withBore then
+      [
+        {
+          name = "bore";
+          patch = "${patchdir}/0001-bore.patch";
         }
 
+      ]
+    else
+      [
 
-  ] else [
+        {
+          name = "glitched-eevdf-additions";
+          patch = "${patchdir}/0003-glitched-eevdf-additions.patch";
+        }
+        {
+          name = "prjc";
+          patch = "${patchdir}/0009-prjc.patch";
+        }
+      ];
 
-     {
-        name = "glitched-eevdf-additions" ;
-        patch  =  "${patchdir}/0003-glitched-eevdf-additions.patch";
-      }
-      {
-      name = "prjc";
-      patch = "${patchdir}/0009-prjc.patch";
-      }
-  ];
+in
 
-  in
-
-
-linuxManualConfig  {
+linuxManualConfig {
   version = "${linux.version}-tkt";
   allowImportFromDerivation = true;
   configfile = "${ksources}/kconfigs/${kversionNoPatch}/config.x86_64";
   inherit (linux) src;
- inherit  stdenv;
- extraMakeFlags = ["LOCALVERSION=-tkt-${compiler.name}_${compiler.version}" ] ++ lib.lists.optionals minimal-modules ["LSMOD=${ksources}/kconfigs/${kversionNoPatch}/minimal-modprobed.db" "localmodconfig"];
- kernelPatches = [
-        {
-        name = "add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER";
-        patch =   "${patchdir}/0001-add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER-by.patch";
-        }
+  inherit stdenv;
+  extraMakeFlags = [
+    "LOCALVERSION=-tkt-${compiler.name}_${compiler.version}"
+  ]
+  ++ lib.lists.optionals minimal-modules [
+    "LSMOD=${ksources}/kconfigs/${kversionNoPatch}/minimal-modprobed.db"
+    "localmodconfig"
+  ];
+  kernelPatches = [
+    {
+      name = "add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER";
+      patch = "${patchdir}/0001-add-sysctl-to-disallow-unprivileged-CLONE_NEWUSER-by.patch";
+    }
 
-        {
-       name = "clear-patches";
-       patch  =  "${patchdir}/0002-clear-patches.patch";
-        }
-        {
-        name = "glitched-base ";
-        patch  =  "${patchdir}/0003-glitched-base.patch";
-        }
-        {
-        name = "glitched-cfs";
-        patch = " ${patchdir}//0003-glitched-cfs.patch";
-        }
-        {
-        name = "add-acs-overrides_iommu";
-        patch = " ${patchdir}/0006-add-acs-overrides_iommu.patch";
-        }
-        {
-      name = "0006-add-acs-";
-      patch = " ${patchdir}//0012-misc-additions.patch";
-        }
-     {
-     name = "OpenRGB";
-     patch = "${patchdir}//0014-OpenRGB.patch";
-     }
+    {
+      name = "clear-patches";
+      patch = "${patchdir}/0002-clear-patches.patch";
+    }
+    {
+      name = "glitched-base";
+      patch = "${patchdir}/0003-glitched-base.patch";
+    }
+    {
+      name = "glitched-cfs";
+      patch = "${patchdir}//0003-glitched-cfs.patch";
+    }
+    {
+      name = "add-acs-overrides_iommu";
+      patch = "${patchdir}/0006-add-acs-overrides_iommu.patch";
+    }
+    {
+      name = "0006-add-acs";
+      patch = "${patchdir}//0012-misc-additions.patch";
+    }
+    {
+      name = "OpenRGB";
+      patch = "${patchdir}//0014-OpenRGB.patch";
+    }
 
-  ] ++ scheduler;
+  ]
+  ++ scheduler;
 
-
-
-
-modDirVersion  = "${lib.versions.pad 3 linux.version}-tkt-${compiler.name}_${compiler.version}";
+  modDirVersion = "${lib.versions.pad 3 linux.version}-tkt-${compiler.name}_${compiler.version}";
   extraMeta = with lib; {
     description = "TꓘT tuned kernel with custom schedulers, bcachefs, OpenRGB tweaks, and memory tuning.";
     homepage = "https://github.com/ETJAKEOC/TKT";
@@ -88,5 +110,5 @@ modDirVersion  = "${lib.versions.pad 3 linux.version}-tkt-${compiler.name}_${com
     maintainers = [ maintainers.yourHandle ];
     platforms = platforms.linux;
 
-    };
+  };
 }
